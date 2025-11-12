cb851e44a3cda0eef49cb7893a9ccbfe7f40c93d
Adam Snape <adam@snape.xyz>
2025-11-12 10:31:51 +0000

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://adamsnape.com',
+  integrations: [react(), sitemap()]
 });
