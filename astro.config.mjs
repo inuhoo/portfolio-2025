c27e67dc0eb24c9f9adfc774e62ce623b6067eed
Adam Snape <adam@snape.xyz>
2025-11-12 10:42:12 +0000

    add a robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adamsnape.com',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap(), robotsTxt()]
 });
